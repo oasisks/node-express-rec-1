9acd9101a5d31859daccd71a3a6d99df11558650
Hao Zhu <haozh@mit.edu>
2023-09-19 00:38:00 -0400

    Fixed the bug with multiple sessions

--- src/concepts/websession.ts
+++ src/concepts/websession.ts
@@ -1,5 +1,5 @@
 // This is (some of) the code for the WebSession concept which was introduced in lecture on 9/18.
-// We're storing the user (in the form of the username string for now) when the user logs in, and we 
+// We're storing the user (in the form of the username string for now) when the user logs in, and we
 // reset the session's user when the user logs out.
 
 import { SessionData } from "express-session";
@@ -28,6 +28,7 @@ export default class WebSessionConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
+    this.isNotActive(session);
     session.user = username;
   }
 
@@ -46,6 +47,12 @@ export default class WebSessionConcept {
   isActive(session: WebSessionDoc) {
     if (session.user === undefined) {
       throw new UnauthenticatedError("Not logged in!");
+    }
+  }
+
+  isNotActive(session: WebSessionDoc) {
+    if (session.user !== undefined) {
+      throw new UnauthenticatedError(`An existing session exists: ${session.user}`);
     }
   }
 }
